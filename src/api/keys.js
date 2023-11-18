d91848fac0fe2bac404daffc743803f09b9d562e
Ilya Bondarchuk <cod3provider@gmail.com>
2023-11-18 19:33:54 +0200

    add constants

---
+++ src/api/keys.js
@@ -0,0 +1,7 @@
+const API_KEY = 'live_TPrcb81nEhbpChqH04IgSnjHpAA3iFbZ94HJ0KBZ5c8W8cRyAEVVHoOGtOTgjuw2';
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+
+export const KEYS = {
+  API_KEY,
+  BASE_URL,
+}
