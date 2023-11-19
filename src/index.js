804607ee3d23ca844bdb37781ac056a56d8e0825
Ilya Bondarchuk <cod3provider@gmail.com>
2023-11-19 22:10:38 +0200

    done main features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import { fetchBreeds } from './api/cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './api/cat-api.js';
 
 const select = document.querySelector('.breed-select');
+const divInfo = document.querySelector('.cat-info');
 
-fetchBreeds()
-.then(data => select.insertAdjacentHTML('afterbegin', createOption(data)))
-.catch(err => console.log(err))
+let selectedId;
 
-function createOption(arr) {
-  return arr.map(
-    ({id, name}) => `
-    <option value=${id}>${name}</option>
-  `
-  )
+function FetchCatByIdDecorator() {
+  fetchCatByBreed(selectedId)
+  .then(data => divInfo.innerHTML = createMarkup(data))
+  .catch(err => console.log(err));
 }
 
-fetchBreeds();
+fetchBreeds()
+.then(data => {
+  select.insertAdjacentHTML('afterbegin', createOptions(data));
+  selectedId = select.value;
+  console.log(selectedId);
+
+  if (!divInfo.length) {
+    FetchCatByIdDecorator();
+  }
+})
+.catch(err => console.log(err));
+
+function createMarkup(data) {
+  const { description, name, temperament } = data.breeds[0];
+
+  return `
+    <img src='${data.url}' alt='${description}'/>
+    <h1>${name}</h1>
+    <p>${description}</p>
+    <p>${temperament}</p>
+  `;
+}
+
+function createOptions(arr) {
+  return arr.map(({ id, name }) => `
+    <option value='${id}'>${name}</option>
+  `);
+}
+
+
+select.addEventListener('change', function() {
+  selectedId = this.value;
+  FetchCatByIdDecorator();
+  console.log(selectedId);
+});
