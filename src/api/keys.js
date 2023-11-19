11323d6f82b3dd09c80587fd4bb03d4f83cb35e9
Ilya Bondarchuk <cod3provider@gmail.com>
2023-11-19 22:09:42 +0200

    add images url

--- src/api/keys.js
+++ src/api/keys.js
@@ -1,7 +1,9 @@
 const API_KEY = 'live_TPrcb81nEhbpChqH04IgSnjHpAA3iFbZ94HJ0KBZ5c8W8cRyAEVVHoOGtOTgjuw2';
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+const IMAGE_URL = 'https://api.thecatapi.com/v1/images/search?breed_ids';
 
-export const KEYS = {
+export {
   API_KEY,
   BASE_URL,
+  IMAGE_URL,
 }
