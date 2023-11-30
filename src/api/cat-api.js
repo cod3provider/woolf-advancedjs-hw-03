f3235f1f7cd5d66b633d32e2e73cb074c19ed460
Ilya Bondarchuk <cod3provider@gmail.com>
2023-11-30 18:31:23 +0200

    clean console.logs

--- src/api/cat-api.js
+++ src/api/cat-api.js
@@ -6,18 +6,14 @@ axios.defaults.headers.common['x-api-key'] = API_KEY;
 function fetchBreeds() {
   return axios(BASE_URL)
   .then(response => {
-    console.log(response);
-
     return response.data;
   })
-  .catch(err => console.log(err.message));
+  .catch(err => console.log(err));
 }
 
 function fetchCatByBreed(breedId) {
   return axios(`${IMAGE_URL}=${breedId}`)
   .then(res => {
-    console.log(res.data[0]);
-
     return res.data[0];
   })
   .catch(err => console.log(err));
